d6f46b1d8cc6e354fd047245e33a5025536fd76b
Laszlo Treszkai <laszlo.treszkai@gmail.com>
2019-08-15 14:34:26 +0200

    Bump version number, add myself as author

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 
 setup(name='best',
-      description='Bayesian estimation supersedes the t test',
-      author='Andrew Straw',
+      description='Bayesian estimation supersedes the t-test',
+      author='Andrew Straw and Laszlo Treszkai',
       author_email='strawman@astraw.com',
       url='https://github.com/strawlab/best',
-      version='1.0',
+      version='2.0',
       packages=['best'],
       license='MIT',
       )
