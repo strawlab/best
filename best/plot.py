fix(minor): Fix effect size formula in plot xlabel

best/plot.py
@@ -186,7 +186,7 @@ def make_figure(M, n_bins=30, group1_name='Group 1', group2_name='Group 2'):
     plot_posterior(effect_size, bins=n_bins, ax=ax10,
                    title='Effect Size',
                    draw_zero=True,
-                   label=r'$(\mu_1 - \mu_2)/\sqrt{(\sigma_1 + \sigma_2)/2}$')
+                   label=r'$(\mu_1 - \mu_2)/\sqrt{(\sigma_1^2 + \sigma_2^2)/2}$')
 
     orig_vals = np.concatenate((M.group1.value, M.group2.value))
     bin_edges = np.linspace(np.min(orig_vals), np.max(orig_vals), 30)