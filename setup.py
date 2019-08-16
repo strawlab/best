78b7bd288fa3ed34a589981d5e072f6cb614ddf0
Laszlo Treszkai <laszlo.treszkai@gmail.com>
2019-08-16 11:20:08 +0200

    setup.py: add long desc. and PyPI classifiers

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup
 
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='best',
       description='Bayesian estimation supersedes the t-test',
       author='Andrew Straw and Laszlo Treszkai',
       author_email='strawman@astraw.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/strawlab/best',
       version='2.0',
       packages=['best'],
+      classifiers=[
+              "Programming Language :: Python :: 3",
+              "License :: OSI Approved :: MIT License",
+              "Operating System :: OS Independent",
+      ],
       license='MIT',
       )
